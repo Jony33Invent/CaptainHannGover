b3ea7fb52e382c24dc27fcdecdb416ff5871ca9f
Jony33Invent <35571614+Jony33Invent@users.noreply.github.com>
2020-05-30 20:17:00 -0300

    Sprites novos adicionados

---
+++ CapHannGover - UnityGame/Assets/Scripts/deathChecker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class deathChecker : MonoBehaviour
+{
+    public GameObject player;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+            player.GetComponent<Transform>().position = new Vector3(-6f,0f,0f);
+    }
+}
