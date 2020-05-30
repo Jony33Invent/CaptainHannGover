d86ba2e1db0a26e351ff5a837d85b04b27d35d80
Jony33Invent <35571614+Jony33Invent@users.noreply.github.com>
2020-05-30 17:32:30 -0300

    Sprites novos adicionados

--- CapHannGover - UnityGame/Assets/Scripts/MoveScript.cs
+++ CapHannGover - UnityGame/Assets/Scripts/MoveScript.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 public class MoveScript : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public float directionAxis;
     public float jumpForce = 12f;
     public bool isGrounded = false;
+    public SpriteRenderer sprite;
+
     void Start()
     {
     }
 
     void Update()
     {
-
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        directionAxis = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(directionAxis, 0f, 0f);
+        if (directionAxis > 0)
+            sprite.flipX = false;
+        else if(directionAxis<0)
+            sprite.flipX = true;
         transform.position += movement * Time.deltaTime * moveSpeed;
         if (Input.GetButtonDown("Jump") && isGrounded)
             Jump();
